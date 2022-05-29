da8e59c4aede49dc5e480814cfc28f562bd964e7
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-05-29 23:46:15 +0200

    formatting

--- src/app/gateway/echo/controller.clj
+++ src/app/gateway/echo/controller.clj
@@ -5,7 +5,7 @@
 
 (defn routes [_]
   ["/echo/:id"
-   { :middleware [wrap-params]
+   {:middleware [wrap-params]
     :get
     (fn [request-map]
       (response/ok (str "path-params: " (:path-params request-map) ", query params: " (:query-params request-map))))}])
